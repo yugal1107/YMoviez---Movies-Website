20ae7914fd44ff2ee87b6594b223c2a19ab32821
yugal1107 <yugal1107@gmail.com>
2025-05-19 19:35:17 +0530

    Done with like feature implementation

--- client/src/services/fetchData.js
+++ client/src/services/fetchData.js
@@ -2,13 +2,10 @@ import axios from "axios";
 
 async function fetchData(url) {
   try {
-    const response = await axios.get(url, {
-      timeout: 10000, // Set a 10-second timeout
-    });
+    const response = await axios.get(url);
     return response.data;
   } catch (error) {
-    console.error("Error occurred while fetching data:", error.message, error.code);
-    throw error; // Rethrow to allow calling code to handle the error
+    console.error("Error occured while fetching data : ", error);
   }
 }
 
